a0cf56b024ac1cb260004a9fe39e8556c129ed31
Fuuun1024 <worapopfong@gmail.com>
2025-02-18 16:21:52 +0700

    lab21 average

--- average.cpp
+++ average.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    double sum = 0;
+    double member = argc-1;
+    
+    if(argc > 1){
+    
+    for(int i = 0; i < member ; i++){
+        sum += atoi( argv[i+1] );
+    }
+    double Average = sum/member;
+    cout << "---------------------------------\n";
+    cout << "Average of "<< member <<" numbers = " << Average << endl;
+    cout << "---------------------------------";
+    
+    }
+    
+    else cout << "Please input numbers to find average.";
+    
+    return 0;
+}
 
